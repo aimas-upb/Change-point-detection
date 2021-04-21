Adding script for running RulSIF parameter grid search; fixing computation error in EachSensorLastActivation

src/features/EachSensorLastActivationFeature.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import numpy as np
 
 from src.features.base.Feature import Feature
 from src.models.Window import Window
@@ -25,11 +26,17 @@ class EachSensorLastActivationFeature(Feature):
 
             current_sensor_time = self.get_last_sensor_activation_from_window(current_sensor_name, window)
             previous_sensor_time = self.main_time_tracker[current_sensor_name]
-
-            dt = current_sensor_time - previous_sensor_time
-            result[index] = round(((dt.seconds * 1e6) + dt.microseconds) / 60 / 1e6, 2)
-            self.main_time_tracker[current_sensor_name] = current_sensor_time
-
+            
+            if current_sensor_time:
+                dt = current_sensor_time - previous_sensor_time
+                result[index] = np.float64(int(dt.seconds) / 60)
+                self.main_time_tracker[current_sensor_name] = current_sensor_time
+            else:
+                current_sensor_time = datetime.strptime(window.events[-1].date + ' ' + window.events[-1].time,
+                                                        self.TIME_FORMAT)
+                dt = current_sensor_time - previous_sensor_time
+                result[index] = np.float64(int(dt.seconds) / 60)
+            
         return result
 
     def get_last_sensor_activation_from_window(self, sensor: str, window: Window):
@@ -38,7 +45,8 @@ class EachSensorLastActivationFeature(Feature):
                 return datetime.strptime(event.date + ' ' + event.time, self.TIME_FORMAT)
 
         # return self.main_time_tracker[sensor]
-        return datetime.strptime(window.events[-1].date + ' ' + window.events[-1].time, self.TIME_FORMAT)
+        # return datetime.strptime(window.events[-1].date + ' ' + window.events[-1].time, self.TIME_FORMAT)
+        return None
 
     def initialize_main_time_tracker(self):
         parser = WindowEventsParser()